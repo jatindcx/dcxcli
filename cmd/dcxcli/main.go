216dcb2d2a9bdf532d5579a09a5b5c0bb30b145b
jatindcx <jatin.salgotra@coindcx.com>
2025-07-11 19:26:26 +0530

    added new mock test file for testing docker pulling

--- cmd/dcxcli/main.go
+++ cmd/dcxcli/main.go
@@ -2,22 +2,12 @@ package main
 
 import (
 	"fmt"
-
-	"dcxcli/internal/generate"
-	"dcxcli/internal/tgscip"
 	"dcxcli/pkg/cli"
-	"dcxcli/pkg/preRun"
-	"dcxcli/pkg/types"
 )
 
 func main() {
 	app := cli.New(nil)
-
-	// adding nested command, e.g. dcxcli tgscip generate
-	// AddCommand needs, use, run, Meta(Short and Long), Init(for initializing flags)
-	app.AddCommand("tgscip", tgscip.DoSomething, types.Meta{}, nil).AddCommand("generate", generate.GeneratePassword, types.Meta{}, generate.Init)
-	app.ApplyPreRun(preRun.SamplePreRun)
-
+	InitService(app)
 	if err := app.Execute(); err != nil {
 		fmt.Println(err)
 	}
